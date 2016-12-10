0ae4d993fdacaf85a863367d96613ff3aaf356e9
Marina Limeira <marinaflessa@gmail.com>
2016-12-09 23:49:04 -0300

    Add current_user from Doorkeeper

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::ApplicationController < ActionController::API
   respond_to :json
+
+  def current_user
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -10,6 +10,11 @@ Doorkeeper.configure do
     #   User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
   end
 
+  resource_owner_from_credentials do |_routes|
+    user = User.find_for_database_authentication(email: params[:username])
+    user if user && user.valid_password?(params[:password])
+  end
+
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
   # admin_authenticator do
   #   # Put your admin authentication logic here.
