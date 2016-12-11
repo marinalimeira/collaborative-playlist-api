08b72aa517d6602ae7ffab5d5a1159c99fa7b7a0
Marina Limeira <marinaflessa@gmail.com>
2016-12-11 03:40:47 -0300

    Fix return status at UsersController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::UsersController < Api::V1::ApplicationController
   def create
     user_service = Services::User::Create.new create_params
-
     if user_service.call
-      render json: user_service.user.to_json, status: :ok
+      render json: user_service.user, status: :ok
     else
-      render json: user_service.user.errors.to_json, status: :unprocessed_entity
+      render json: user_service.user.errors.full_messages, status: :unprocessable_entity
     end
   end
 
