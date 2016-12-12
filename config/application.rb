4bb86a5406b048a85ab5dc07ab6dc4609c5b428d
Marina Limeira <marinaflessa@gmail.com>
2016-12-12 03:28:54 -0300

    Set cors config
    - Add password doorkeeper grant type

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,12 @@ module SpaceshipParkingLotApi
 
     config.autoload_paths << Rails.root.join("app")
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -11,7 +11,7 @@ Doorkeeper.configure do
   end
 
   resource_owner_from_credentials do |_routes|
-    user = User.find_for_database_authentication(email: params[:username])
+    user = User.find_for_database_authentication(email: params[:email])
     user if user && user.valid_password?(params[:password])
   end
 
@@ -103,7 +103,7 @@ Doorkeeper.configure do
   #   http://tools.ietf.org/html/rfc6819#section-4.4.2
   #   http://tools.ietf.org/html/rfc6819#section-4.4.3
   #
-  # grant_flows %w(authorization_code client_credentials)
+  grant_flows %w(password)
 
   # Under some circumstances you might want to have applications auto-approved,
   # so that the user skips the authorization step.
