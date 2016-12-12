9c39562f46606add8e1c5d2b7eeeb71e6dcd132b
Marina Limeira <marinaflessa@gmail.com>
2016-12-12 03:29:21 -0300

    Add User serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,13 +4,13 @@ class Api::V1::UsersController < Api::V1::ApplicationController
     if user_service.call
       render json: user_service.user, status: :ok
     else
-      render json: user_service.user.errors.full_messages, status: :unprocessable_entity
+      render json: user_service.user.errors, status: :unprocessable_entity
     end
   end
 
   private
 
-  def create_params 
+  def create_params
     params.permit :name, :email, :password, :password_confirmation
   end
 end
